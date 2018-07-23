46aad15e9f49a57f09e4de9cd2b14b005bbffa29
Todd Tyree <tatyree@gmail.com>
2018-07-23 12:26:07 +0100

    fixup! fixup! WIP: Add most basic sinatra app for testing

--- webhook.rb
+++ webhook.rb
@@ -9,7 +9,7 @@ GITHUB_PERSONAL_ACCESS_TOKEN = ENV.fetch('GITHUB_PERSONAL_ACCESS_TOKEN')
 @client = Octokit::Client.new(:access_token => GITHUB_PERSONAL_ACCESS_TOKEN)
 
 get '/heartbeat' do
-  { labelling_webhook: 'ping' }.to_json
+  json labelling_webhook: 'ping'
 end
 
 post '/webhook' do
