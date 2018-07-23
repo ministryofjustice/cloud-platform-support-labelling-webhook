a860ce7ed72b098abda4de24d8320f33c2dfb516
Todd Tyree <tatyree@gmail.com>
2018-07-23 11:55:57 +0100

    Add puma as webserver
    Mainly for efficiency and consistency-this is generally how we run all
    our rack-based services on heroku.

--- webhook.rb
+++ webhook.rb
@@ -1,10 +1,16 @@
-require 'sinatra'
 require 'json'
+require 'octokit'
 require 'pp'
+require 'sinatra'
+require 'sinatra/reloader' if development?
 
 GITHUB_PERSONAL_ACCESS_TOKEN = ENV.fetch('GITHUB_PERSONAL_ACCESS_TOKEN')
 
 @client = Octokit::Client.new(:access_token => GITHUB_PERSONAL_ACCESS_TOKEN)
+
+get '/heartbeat' do
+  { webhook: 'ping' }.to_json
+end
 
 post '/webhook' do
   pp JSON.parse(params)
