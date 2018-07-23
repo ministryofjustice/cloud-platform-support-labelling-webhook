dc97396621b0452dedcc897c5e693f17c46720cf
Todd Tyree <tatyree@gmail.com>
2018-07-23 12:24:17 +0100

    WIP: Add most basic sinatra app for testing
    Creates and extends a sinatra app that will be a webhook endpoints for a
    GitHub webhook. The idea is to define an endpoint `/webhook` that takes
    in issue data from Github when a new issue is created and adds a label
    to issues that match a particular regex.
    
    To make this work we need to define a regex, setup access to the github
    API, parse the incoming json payload and add the label. This code does
    that.

--- webhook.rb
+++ webhook.rb
@@ -1,17 +1,29 @@
 require 'json'
 require 'octokit'
-require 'pp'
 require 'sinatra'
+require 'sinatra/json'
 require 'sinatra/reloader' if development?
 
 GITHUB_PERSONAL_ACCESS_TOKEN = ENV.fetch('GITHUB_PERSONAL_ACCESS_TOKEN')
+#REPO = 'ministryofjustice/cloud-platform'
+#BUG_REPORT_REGEX = %r{Service name}
+REPO = 'tatyree/can-only-draw-pigs'
+BUG_REPORT_REGEX = %r(impact on the service)i
 
-@client = Octokit::Client.new(:access_token => GITHUB_PERSONAL_ACCESS_TOKEN)
 
 get '/heartbeat' do
-  { webhook: 'ping' }.to_json
+  json labelling_webhook: 'ping'
 end
 
 post '/webhook' do
-  pp JSON.parse(params)
+  push = JSON.parse(request.body.read)
+  action = push['action']
+  issue = push.fetch('issue', {})
+
+  if action == 'opened' && !issue['number'].nil? && issue['body']&.match(BUG_REPORT_REGEX)
+    client = Octokit::Client.new(:access_token => GITHUB_PERSONAL_ACCESS_TOKEN)
+    client.add_labels_to_an_issue(REPO, Integer(issue['number']), ['bug'])
+  end
+
+  status :ok
 end
